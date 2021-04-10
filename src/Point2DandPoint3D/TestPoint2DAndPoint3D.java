64235c3f32ebb4177e710010c3ccc32f801f3644
VoHoangDuong <duong6046237@gmail.com>
2021-04-10 12:23:11 +0700

    update

---
+++ src/Point2DandPoint3D/TestPoint2DAndPoint3D.java
@@ -0,0 +1,10 @@
+package Point2DandPoint3D;
+
+public class TestPoint2DAndPoint3D {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D(2,5);
+        System.out.println(point2D.toString());
+        Point3D point3D = new Point3D(2,1,6);
+        System.out.println(point3D.toString());
+    }
+}
--- src/fruit/Apple.java
+++ src/fruit/Apple.java
@@ -1,7 +1,6 @@
 package fruit;
 
 public class Apple extends Fruit {
-
     @Override
     public String howtoEat() {
         return "Apple could be slided";
